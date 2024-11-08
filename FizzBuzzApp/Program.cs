390fa41bd010b4924054a852da5b4d35dcaf29c5
Asia Lie <asia.lie@softwire.com>
2024-11-08 09:41:23 +0000

    Change FOR loop condition

--- FizzBuzzApp/Program.cs
+++ FizzBuzzApp/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-for (int i = 1; i < 101; i++) {
+for (int i = 1; i <= 100; i++) {
     if (i % 3 == 0 && i % 5 == 0) {
         Console.WriteLine("FizzBuzz");
     }
